8bf75bd1cafc1849a8ff29fedcf088fb8499a000
thurayaalz <thuraya0alz@gmail.com>
2025-08-29 07:21:39 +0300

    basic functions for add and render books

--- lib.js
+++ lib.js
@@ -1,39 +1,42 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
-  
-
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
-  const book = document.createElement("div");
-  book.classList.add("book");
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function renderB(){
-    const shelf = document.querySelector('.shelf');
-    shelf.innerHTML = '';
+function addBookToLibrary(Book) {
+  // take params, create a book then store it in the array
+  myLibrary.push(bookObj);
+  renderB();
+}
 
-    myLibrary.forEach((book , index)=>{
-        const book = document.createElement('div');
-        book.classList.add('book');
-        book.innerText = `<h3> ${book.title}</h3>
+function renderB() {
+  const shelf = document.querySelector(".shelf");
+  shelf.innerHTML = "";
+
+  myLibrary.forEach((book, index) => {
+    const book = document.createElement("div");
+    book.classList.add("book");
+    book.innerText = `<h3> ${book.title}</h3>
         <p>${book.authur}</p>
         <p> ${book.pages}</p>
-        <p>Status ${book.read? 'Read' : 'Not Read Yet'}</p> 
-        <button onclick="removeBook(${index})" id="DB">Remove Book</button> `
-        shelf.appendChild(book);
-    });
+        <p>Status ${book.read ? "Read" : "Not Read Yet"}</p> 
+        <button onclick="removeBook(${index})" id="DB">Remove Book</button> `;
+    shelf.appendChild(book);
+  });
+}
+function removeBook(index){
+myLibrary.splice(index , 1);
+renderB();
 }
 
 // BACKGROUND -------------------------------------------------------
 
-document.addEventListener('DOMContentLoaded', () => {
-  const Bub = document.querySelector('.inter');
+document.addEventListener("DOMContentLoaded", () => {
+  const Bub = document.querySelector(".inter");
   let cX = 0;
   let cY = 0;
   let tgX = 0;
@@ -42,14 +45,15 @@ document.addEventListener('DOMContentLoaded', () => {
   function move() {
     cX += (tgX - cX) / 20;
     cY += (tgY - cY) / 20;
-    Bub.style.transform =
-      `translate(${Math.round(cX)}px , ${Math.round(cY)}px)`;
+    Bub.style.transform = `translate(${Math.round(cX)}px , ${Math.round(
+      cY
+    )}px)`;
     requestAnimationFrame(() => {
       move();
     });
   }
 
-  window.addEventListener('mousemove', (event) => {
+  window.addEventListener("mousemove", (event) => {
     tgX = event.clientX;
     tgY = event.clientY;
   });
